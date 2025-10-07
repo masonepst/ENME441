359e44cb3d0ac76b96d20f0dd074a94de1229777
masonepst <masonepst@gmail.com>
2025-10-07 19:03:32 -0400

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-# LED order 2,3,4,17,27,22,14,15,18,23
+# LED = [2,3,4,17,27,22,14,15,18,23]
 
 import RPi.GPIO as GPIO
 import time
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 
 pwm = GPIO.PWM(2,500)
+pwm2 = GPIO.PWM(3,500)
 pwm.start(0)
 timer = time.time()
 f = 0.2
@@ -20,6 +21,11 @@ while True:
 	brightness = B*100
 
 	pwm.ChangeDutyCycle(brightness)
+
+	B2 = math.sin(2*math.pi*f*t-math.pi/11)
+	B2 = B2**2
+	brightness2 = B2*100
+	pwm2.ChangeDutyCycle(brightness2)
 
 pwm.stop()
 GPIO.cleanup()
